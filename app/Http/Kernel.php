8c475f732818251824b8482e991fd767c6ef0e2b
A7medFlex <om5280201@gmail.com>
2022-02-21 20:56:11 +0200

    editedddddd

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,16 +29,16 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        // 'web' => [
-        //     \App\Http\Middleware\EncryptCookies::class,
-        //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        //     \Illuminate\Session\Middleware\StartSession::class,
-        //     // \Illuminate\Session\Middleware\AuthenticateSession::class,
-        //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        //     // \App\Http\Middleware\VerifyCsrfToken::class,
-        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        //     \App\Http\Middleware\languags::class,
-        // ],
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\languags::class,
+        ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
