959af4f70726a84cb47b86e140d3bd4342b567ca
A7medFlex <om5280201@gmail.com>
2022-02-21 16:27:25 +0200

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Credit</title>
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+        <link rel="stylesheet" href="/css/main.css">
         <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css">
     </head>
 
