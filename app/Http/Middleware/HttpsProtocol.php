2dfc4cda8426a983ed3b9cbbdf8df48f82fdd1d7
A7medFlex <om5280201@gmail.com>
2022-02-22 16:57:58 +0200

    upddd

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace MyApp\Http\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
-class HttpsProtocol {
-
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-            if (!$request->secure() && App::environment() === 'production') {
-                return redirect()->secure($request->getRequestUri());
-            }
+        if (!$request->secure() && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
 
-            return $next($request);
+        return $next($request);
     }
 }
